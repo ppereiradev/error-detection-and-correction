17938985f39fb262fd649b5227bd9b9b73a55472
Paulo Pereira <ppereira.dev@gmail.com>
2024-09-01 18:42:21 -0300

    remove some methods in two-dimensional parity algorithm

--- error-detection/algorithms/two_dimensional_parity.py
+++ error-detection/algorithms/two_dimensional_parity.py
@@ -7,23 +7,9 @@ During transmission or storage, if an error alters the number of bits,
 the parity bit will be used to identify whether there is an error or not.
 """
 class TwoDimensionalParity:
-    def find_parity(self, data):
-        pass
-
-    def has_even_parity(self, data):
-        pass
-
-    def has_odd_parity(self, data):
-        pass
-
     def encode_even_data(self, data):
-        pass
-
-    def encode_odd_data(self, data):
         pass
 
     def decode_even_data(self, data):
         pass
 
-    def decode_odd_data(self, data):
-        pass
